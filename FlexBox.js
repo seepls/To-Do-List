9b933e688e3420f01464a1010cf190e0ac6f5e95
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-12-04 14:42:36 +0530

    Create FlexBox.js

---
+++ FlexBox.js
@@ -0,0 +1,25 @@
+it is a CSS layout system that provides consistency in your layout across different screen sizes !!!
+default flex direction : column 
+flex parameter only supports only single number as its value in react - native
+
+if its number is positive then the component that has this prop becomes flexible 
+
+flex-Direction : four options 
+- row
+- row-reverse
+- column
+- column-reverse
+ direction that the children of flex container will be laid out in
+ 
+ 
+ import React , {Component} from 'react';
+ 
+ importing Component in curly braces callled destructing assignment .extracts specfic data from an array or object into a variable
+ 
+ var replaced by : let and const
+ let : declares a block scoped variable. mutable.
+ const : block scoped bt immutable
+ 
+ 
+ what does exporting modules means ?
+ 
